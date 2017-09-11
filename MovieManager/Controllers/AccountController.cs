813f4c63ab8546817530db409cb6b1ed3622d403
nu <pakorn@2c2p.com>
2017-09-11 22:21:16 +0700

    Test Add Comment#2

--- MovieManager/Controllers/AccountController.cs
+++ MovieManager/Controllers/AccountController.cs
@@ -55,6 +55,7 @@ namespace MovieManager.Controllers
         public IActionResult Login(string returnUrl = null)
         {
             //Test Add Comment#1
+            //Test Add Comment#2
 
             ViewData["ReturnUrl"] = returnUrl;
             return View();

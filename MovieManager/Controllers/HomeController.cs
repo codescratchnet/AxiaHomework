1fe7998d615732386d7fd0ade1d7bd4589db6102
nu <pakorn@2c2p.com>
2017-09-15 22:59:55 +0700

    back at one

--- MovieManager/Controllers/HomeController.cs
+++ MovieManager/Controllers/HomeController.cs
@@ -10,9 +10,6 @@ namespace MovieManager.Controllers
     {
         public IActionResult Index()
         {
-            //Add Comment #1
-            //Add Comment #2
-            //Add Comment #3
             return View();
         }
 
